72964f6f89365c70b8c7ba5db815b69bf10cebbc
sümeyye kılıç <smyyekilic@gmail.com>
2019-12-04 02:04:33 +0300

    sss4

--- dfas/models.py
+++ dfas/models.py
@@ -7,7 +7,6 @@ class QueryModel(models.Model):
 
 	location = models.CharField(max_length=150, verbose_name='Konum Bilgisi')
 	venue = models.CharField(max_length=150, verbose_name='Mekan Türü')
-	user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 	limit = models.CharField(max_length=40, verbose_name='Listelenecek Limit.')
 
 	def __str__(self):
