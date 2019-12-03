3cd186b0162bde09c3db03af986c5b8388ee839f
sümeyye kılıç <smyyekilic@gmail.com>
2019-12-04 01:55:46 +0300

    sss3

--- dfAPI/settings.py
+++ dfAPI/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATICFILES_DIRS = [
